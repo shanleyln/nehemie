ad8528920031eba4796438e6e819b754896a3637
shanleyln <shanleylipeme17@email.com>
2025-09-12 16:09:49 +0100

    ok

--- resources/views/admin/pvit/secret.blade.php
+++ resources/views/admin/pvit/secret.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <title>Clé secrète MyPVit</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
