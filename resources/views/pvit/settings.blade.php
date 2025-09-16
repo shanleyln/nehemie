7012acceecfbb0174835025c232aedb7aba7f72e
shanleyln <shanleylipeme17@email.com>
2025-09-16 16:22:36 +0100

    ok

--- resources/views/pvit/settings.blade.php
+++ resources/views/pvit/settings.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.app')
 
 @section('content')
-    <div class="container py-4">
+    <div class="py-4">
         @if (session('success'))
             <div class="alert alert-success">{{ session('success') }}</div>
         @endif
