50e4d49054b666837377ef4ccb5f79e4e709b9bc
shanleyln <shanleylipeme17@email.com>
2025-09-16 15:56:45 +0100

    ok

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -47,7 +47,7 @@
 
     <!-- header end -->
     <!-- Main content -->
-    <div class="main-content" style="margin-top: 100px;">
+    <div class="main-content" style="margin-top: 300px;">
         @yield('content')
     </div>
 
