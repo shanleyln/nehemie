fe0a56dc9c8ede31f87bdb23e96ecaec325a67ef
shanleyln <shanleylipeme17@email.com>
2025-09-18 16:06:51 +0100

    ok

--- resources/views/livewire/actualites.blade.php
+++ resources/views/livewire/actualites.blade.php
@@ -178,7 +178,7 @@
         </div>
     </div> --}}
 
-    <div class="container">
+    <div class="container py-5">
         <div class="taggbox" style="width:100%;height:100%;overflow:auto;" data-widget-id="300959" data-website="1">
         </div>
         <script src="https://widget.taggbox.com/embed.min.js" type="text/javascript"></script>
