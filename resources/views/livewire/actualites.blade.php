78d1863a8c34f4a8d7f4486523732bdcb79a8ccc
shanleyln <shanleylipeme17@email.com>
2025-09-18 15:51:13 +0100

    ok

--- resources/views/livewire/actualites.blade.php
+++ resources/views/livewire/actualites.blade.php
@@ -178,6 +178,12 @@
         </div>
     </div>
 
+    <div>
+        <div class="taggbox" style="width:100%;height:100%;overflow:auto;" data-widget-id="300959" data-website="1">
+        </div>
+        <script src="https://widget.taggbox.com/embed.min.js" type="text/javascript"></script>
+    </div>
+
     @if ($showModal)
         <div class="modal-backdrop fade show"></div>
     @endif
