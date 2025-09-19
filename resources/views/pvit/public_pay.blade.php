e66ea65c552df83a0b4bcf0523bf76f8b909d9c6
shanleyln <shanleylipeme17@email.com>
2025-09-19 13:25:51 +0100

    ok

--- resources/views/pvit/public_pay.blade.php
+++ resources/views/pvit/public_pay.blade.php
@@ -78,7 +78,7 @@
 
 <body>
 
-    <div class="custom-container">
+    <div class="custom-container mt-5">
         <!-- Onglets principaux -->
         <ul class="nav nav-tabs nav-justified mb-3 shadow-sm rounded bg-white" id="beneficiaireTabs" role="tablist">
             <li class="nav-item" role="presentation">
