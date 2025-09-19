f4a2322f98ee7f130b6b6e879c6bbbba93beaa5f
shanleyln <shanleylipeme17@email.com>
2025-09-19 11:37:15 +0100

    ok

--- resources/views/livewire/nehemie/chat-assistant.blade.php
+++ resources/views/livewire/nehemie/chat-assistant.blade.php
@@ -1,4 +1,6 @@
 {{-- resources/views/livewire/nehemie/chat-assistant.blade.php --}}
 <div class="fixed right-5 bottom-24 z-50" wire:ignore>
-    <x-modules.chatbot />
+
+    @include('modules.chatbot')
+
 </div>
