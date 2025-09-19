a92c311bd47aa8341606c56c7e33db2347589f19
shanleyln <shanleylipeme17@email.com>
2025-09-19 10:52:46 +0100

    ok

--- resources/views/layouts/nehemie-mobile.blade.php
+++ resources/views/layouts/nehemie-mobile.blade.php
@@ -3,10 +3,16 @@
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>@yield('title', 'NÉHÉMIE International - App Mobile')</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="n8n-session-id" content="{{ session()->getId() }}">
+    <meta name="theme-color" content="#fff">
+    <title>@yield('title')</title>
 
+    <!-- Icône du site -->
+    <link rel="icon" href="{{ asset('images/logo2.png') }}" type="image/x-icon">
     {{-- Tailwind & FontAwesome --}}
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.0/css/all.min.css">
