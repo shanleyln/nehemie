a406cf9d17ca77e94f63fde69dacc27bfe1a30f0
shanleyln <shanleylipeme17@email.com>
2025-09-19 10:49:07 +0100

    ok

--- resources/views/layouts/nehemie-mobile.blade.php
+++ resources/views/layouts/nehemie-mobile.blade.php
@@ -113,7 +113,7 @@
         style="position:fixed;top:0;left:0;height:3px;width:0;
 background:linear-gradient(90deg,#1e40af,#3b82f6);z-index:2000;">
     </div>
-
+    @include('modules.chatbot')
     <main class="content px-3">
         {{ $slot }}
     </main>
