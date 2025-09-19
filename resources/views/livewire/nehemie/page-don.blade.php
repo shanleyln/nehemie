aa8f21af1fd114d7366ba004e85eabaec9578a5d
shanleyln <shanleylipeme17@email.com>
2025-09-19 13:09:58 +0100

    ok

--- resources/views/livewire/nehemie/page-don.blade.php
+++ resources/views/livewire/nehemie/page-don.blade.php
@@ -1,6 +1,5 @@
 {{-- resources/views/livewire/nehemie/page-don.blade.php --}}
-<div>
-    <div class="card mb-4">
-        @include('pvit.public_pay')
-    </div>
+
+<div class="card mb-4">
+    @include('pvit.public_pay')
 </div>
